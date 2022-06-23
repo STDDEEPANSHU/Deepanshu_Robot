8a5598bd854aa40cf72eaef740c72e40b572ce4d
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-06-23 21:29:09 +0530

    Update json.py

--- FallenRobot/modules/json.py
+++ FallenRobot/modules/json.py
@@ -1,6 +1,6 @@
 import io
-from FallenRobot.events import register
-from FallenRobot import telethn as tbot
+from DeepanshuRobot.events import register
+from DeepanshuRobot import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions
