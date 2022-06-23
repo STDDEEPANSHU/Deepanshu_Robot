6949d668615136d3990cd1c11898a4def72ebcee
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-06-23 21:30:05 +0530

    Update imdb.py

--- FallenRobot/modules/imdb.py
+++ FallenRobot/modules/imdb.py
@@ -1,11 +1,11 @@
-from FallenRobot import telethn as tbot
+from DeepanshuRobot import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from FallenRobot.events import register
+from DeepanshuRobot.events import register
 
 langi = "en"
 
