566567a461f690fa6a9598a8497519031b1e5f2f
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-06-23 22:33:49 +0530

    Update cleaner.py

--- FallenRobot/modules/cleaner.py
+++ FallenRobot/modules/cleaner.py
@@ -1,14 +1,14 @@
 import html
 
-from FallenRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import (
+from DeepanshuRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from DeepanshuRobot.modules.disable import DisableAbleCommandHandler
+from DeepanshuRobot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from FallenRobot.modules.sql import cleaner_sql as sql
+from DeepanshuRobot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
