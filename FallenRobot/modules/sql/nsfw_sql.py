3c206b23167ec3eeb00e92cae0d0473fcdab146c
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-06-23 23:34:22 +0530

    Update nsfw_sql.py

--- FallenRobot/modules/sql/nsfw_sql.py
+++ FallenRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from FallenRobot.modules.sql import BASE, SESSION
+from DeepanshuRobot.modules.sql import BASE, SESSION
 
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
