6cb2fe8192dc385e634dd918b1b17f19938054df
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-06-23 13:54:07 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Fallen Robot
-:: Enables virtual env mode and then starts Fallen
-env\scripts\activate.bat && py -m FallenRobot
+TITLE STD X Robot
+:: Enables virtual env mode and then starts STD
+env\scripts\activate.bat && py -m STDXRobot
